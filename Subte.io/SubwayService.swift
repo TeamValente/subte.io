d5f7cca09b4f17b87aca8c4f107a3a1ce5e3e868
Mariano Molina <xsin.crueldadx@gmail.com>
2016-06-14 21:12:50 -0300

    Add http services

---
+++ Subte.io/SubwayService.swift
@@ -0,0 +1,15 @@
+//
+//  SubwayService.swift
+//  Subte.io
+//
+//  Created by Mariano Molina on 14/6/16.
+//  Copyright © 2016 marianomolina.xyz. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class SubwayService {
+    // TODO: service methods
+}
